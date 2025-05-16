ef404cedf9006126e4f7de0fe92f4dac3920c67f
FedericoBecchi <federicobecchi10@gmail.com>
2025-05-16 18:42:20 +0200

    Aggiunto il file main.cpp

---
+++ 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// TODO struttura dati  processo ove ogni processo ha tempo di arrivo e burst turnaround time e waiting time come parametri
+// TODO struttura dati coda per avere i processi in esecuzione 
+
+
+/* FLUSSO CODICE LO PENSO COSI INSERISCO TUTTI I PROCESSI NELLA CODA 
+    faccio un'iterazione ovvero computo il processo che è arrivato per primo nella coda
+    fino a che la coda non è vuota.
+    ad ogni iterazione (esecuzione di un processo) aggiorno il turaroudn time e il waiting time
+    quando tutti i processi sono stati eseguiti calcolo le statistiche
+*/
+
+
+
+int main(){
+
+}
