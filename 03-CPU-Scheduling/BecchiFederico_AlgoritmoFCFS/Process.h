f77cbdd99cf01aa2dac24e2b7faa15b00b9d7615
FedericoBecchi <federicobecchi10@gmail.com>
2025-05-16 19:37:08 +0200

    added extend_queuqe function and little things

--- 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/Process.h
+++ 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/Process.h
@@ -6,12 +6,21 @@ class Process{
 
         using time_unit = int; // TODO Clock Cycle <1s ?  
 
-        const time_unit arrival_time;
-        const time_unit burst_time;
+        time_unit arrival_time;
+        time_unit burst_time;
         time_unit turnaround_time;
         time_unit waiting_time;
 
     public:
+
+
+        Process(){
+            this->turnaround_time = 0;
+            this->waiting_time = 0;
+            this->arrival_time = 0;
+            this->burst_time = 0;
+        }
+
         Process(time_unit arv_time , time_unit brst_time) : arrival_time(arv_time) , burst_time(brst_time){
             this->turnaround_time = 0;
             this->waiting_time = 0;
--- 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/QuequeProc.h
+++ 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/QuequeProc.h
@@ -8,16 +8,29 @@ class QuequeProc{
         Process* queque;
         int capacity;
         int n_process;
+
+        void extend_queue() {
+            int new_capacity = this->capacity * 2;
+            Process* new_queue = new Process[new_capacity];
+            
+            for(int i = 0; i < n_process; i++) {
+                new_queue[i] = queque[i];
+            }
+            
+            delete[] queque;
+            queque = new_queue;
+            capacity = new_capacity;
+        }
     public:
         QuequeProc(){
-            this->capacity = 0;
-            this->queque = nullptr;
+            this->capacity = 10;
+            this->queque = new Process[this->capacity];
             this->n_process = 0;
         }
 
         void addProcess(const Process& proc){
             if(this->capacity == this->n_process)
-                //extend queque with function TODO
+                this->extend_queue();
             
             this->queque[this->n_process] = proc;
 
@@ -25,13 +38,14 @@ class QuequeProc{
         }
 
         Process dequee(){
-            if(this->isEmpty())
-                // TODO EXECPTION
+            if(this->isEmpty()){
+                throw "BUCATO"; // TODO CORRECT EXCPTION
+            }
             
             Process old_proc = queque[0];
 
             for(int i = 0 ; i < n_process - 1; ++i)
-                queque[i] = queque[i + 1]; //TODO WRITE PROCESS OPERATOR CONSTRUCTOR
+                queque[i] = queque[i + 1];
 
             n_process--;
 
