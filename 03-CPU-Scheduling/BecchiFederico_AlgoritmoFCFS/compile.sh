5cab8236dbc117cd05d7a01f2628bab8e9fbd43a
BecksFedeUniPr <federico.becchi@studenti.unipr.it>
2025-05-17 11:52:37 +0200

    fixed branch

--- 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/compile.sh
+++
@@ -1,2 +0,0 @@
-# Rember to give the script execution permission with chmod +x compile.sh
-g++ main.cpp Process.cpp QuequeProc.cpp -o ./bin/main.out
--- 03-CPU-Scheduling/BecchiFederico_AlgoritmoFCFS/run.sh
+++
@@ -1,2 +0,0 @@
-# Rember to give the script execution permission with chmod +x run.sh
-./bin/main.out
