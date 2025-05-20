c24fe047bf16796514c9823672ee9a42a3c23a7a
BecksFedeUniPr <federico.becchi@studenti.unipr.it>
2025-05-20 21:51:51 +0200

    test

--- 01-Processes/BecksFede/main.c
+++ 01-Processes/BecksFede/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <string.h>
-#include <unistd.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(){
     int shmid;
